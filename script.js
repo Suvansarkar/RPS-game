e9eab0fd70dbf89ff17cb6238b2662ae061721ac
Suvansarkar <suvansarkar1@gmail.com>
2023-08-14 13:07:58 +0530

    simple rps implementation

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+function getComputerChoice(){
+    const choices = ["Rock", "Paper", "Scissors"];
+    const randint = Math.floor(Math.random()*3);
+    return choices[randint];
+}
 
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    if(playerSelection===computerSelection.toLowerCase()){
+        return "It's a Draw!";
+    }else if(playerSelection==="rock" && computerSelection==="Scissors"){
+        return "You win! " + playerSelection + " beats " + computerSelection + ".";
+    }else if(playerSelection==="scissors" && computerSelection==="Paper"){
+        return "You win! " + playerSelection + " beats " + computerSelection + ".";
+    }else if(playerSelection==="paper" && computerSelection==="Rock"){
+        return "You win! " + playerSelection + " beats " + computerSelection + ".";
+    }else {
+        return "You lose! " + computerSelection + " beats " + playerSelection + ".";
+    }
+}
+
+let flag = true;
+while(flag){
+    const playerSelection = prompt("Enter your choice: ");
+    if(playerSelection==null){
+        flag=false;
+    }
+    console.log(playRound(playerSelection, getComputerChoice()));
+}
